45f38b7efcd23bf7f390e388d7857a30c451d739
Cameron Dutro <camertron@gmail.com>
2022-08-11 21:58:10 -0700

    Wire up to_s method

--- ext/vitesse/vitesse.c
+++ ext/vitesse/vitesse.c
@@ -292,6 +292,7 @@ void Init_vitesse() {
     rb_define_method(buffer_class, "safe_append=", RUBY_METHOD_FUNC(vt_safe_append), 1);
     rb_define_method(buffer_class, "safe_expr_append=", RUBY_METHOD_FUNC(vt_safe_append), 0);
 
+    rb_define_method(buffer_class, "to_s", RUBY_METHOD_FUNC(vt_to_s), 0);
     rb_define_method(buffer_class, "to_str", RUBY_METHOD_FUNC(vt_to_str), 0);
     rb_define_method(buffer_class, "length", RUBY_METHOD_FUNC(vt_length), 0);
     rb_define_method(buffer_class, "empty?", RUBY_METHOD_FUNC(vt_empty), 0);
