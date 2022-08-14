f1d48ac96bdf605d07db81d1167e90f3c92069b2
Cameron Dutro <camertron@gmail.com>
2022-08-13 23:03:47 -0700

    Move loading to top-level method; move version.rb

--- lib/vitesse.rb
+++ lib/vitesse.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+module Vitesse
+  def self.load!
+    require "action_view/buffers"
+    require File.expand_path(File.join("..", "ext", "vitesse", "vitesse"), __dir__)
+    ActionView::OutputBuffer.prepend(Vitesse::OutputBufferPatch)
+    ActionView::RawOutputBuffer.prepend(Vitesse::RawOutputBufferPatch)
+  end
+end
+
 if defined?(Rails::Engine)
   require "vitesse/engine"
 end
--- lib/vitesse/engine.rb
+++ lib/vitesse/engine.rb
@@ -4,17 +4,13 @@ require "rails"
 
 module Vitesse
   class Engine < Rails::Engine
-    config.vitesse = ActiveSupport::OrderedOptions.new(enabled: false);
+    config.vitesse = ActiveSupport::OrderedOptions.new(enabled: true)
 
     initializer "vitesse.load" do |app|
-      options = app.config.view_component
+      options = app.config.vitesse
 
       ActiveSupport.on_load(:action_view) do
-        if options.enabled
-          require File.expand_path(File.join("..", "..", "ext", "vitesse", "vitesse"), __dir__)
-          ActionView::OutputBuffer.prepend(Vitesse::OutputBufferPatch)
-          ActionView::RawOutputBuffer.prepend(Vitesse::RawOutputBufferPatch)
-        end
+        Vitesse.load! if options.enabled
       end
     end
   end
